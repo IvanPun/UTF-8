f5d6e8c251b75f53b1d1f8d66018a7f90d727ccf
Ivan_Pun <whitetiger123asd@gmail.com>
2023-02-11 21:58:14 +0800

    update

--- login.js
+++ login.js
@@ -56,3 +56,35 @@ $(document).ready(function(){
         localStorage.setItem("password", "");
     }
 })
+
+
+//register
+$("#register").click(function(){
+    var name=$("#logname").val();
+    var email=$("#logemail").val();
+    var password=$('#logpass').val();
+    if (name!="" && password!=""&&email!="") {
+        const Url='https://2b87aafb.r2.cpolar.top/InsertUser/'+name+'/'+password+'/'+email;
+        $.ajax({
+            url: Url,
+            type: "GET",
+            success:function(result){
+                console.log(result);
+                if(result=="注册成功!"){
+                    localStorage.setItem("name", name);
+                    localStorage.setItem("password", password);
+                    alert("welcome!");
+                    location.href=("index.html");
+                }else if(result=="用户名重复"){
+                    $("#warning").html("<h5>用户名重复</h5>");
+                }
+            }
+        })
+    }else if(name==''&&password==''){
+        $("#warning").html("<h5>用戶名和密码都不能空缺！</h5>");
+      }else if(name==''){
+        $("#warning").html("<h5>用戶名不能空缺！</h5>");
+      }else{
+        $("#warning").html("<h5>密码不能空缺！</h5>");
+      }
+})
