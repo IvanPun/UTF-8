f8070738b5db837b7db68c59ad27e87dfd8e24d3
zerowinter0 <109864477+zerowinter0@users.noreply.github.com>
2023-02-11 22:54:12 +0800

    上传后端springboot文件夹

---
+++ UTFproject/src/main/java/alex/UTFProject/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package alex.UTFProject.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import alex.UTFProject.entity.Member;
+@Mapper
+public interface MemberMapper {
+    String changeMemberNameById(@Param("id") int id,@Param("name") String name);
+    String changeMemberSignatureById(@Param("id")int id,@Param("signature") String signature);
+    String changeMemberContact_WayById(@Param("id")int id,@Param("contact_way") String contact_way);
+    String changeMemberHobbyById(@Param("id")int id,@Param("hobby") String hobby);
+    String changeMemberPicUrlById(@Param("id")int id,@Param("picUrl") String picUrl);
+    String changeMemberWebById(@Param("id")int id,@Param("web") String web);
+    Member getMemberById(@Param("id")int id);
+
+}
