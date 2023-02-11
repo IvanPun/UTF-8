62bdb90b73ba4d4c6a653d2fbee7e5e57983f5a4
Ivan_Pun <whitetiger123asd@gmail.com>
2023-02-11 22:47:34 +0800

    update

---
+++ changePassword.js
@@ -0,0 +1,28 @@
+//check
+$("#check").click(function(){
+    var name=$("#logname").val();
+    var password=$('#logpass').val();
+    var email=$('#logemail').val();
+    if (name!="" && password!=""&& email!="") {
+        const Url='https://2b87aafb.r2.cpolar.top/changeUserPassword/'+name+'/'+email+'/'+password;
+        $.ajax({
+            url: Url,
+            type: "GET",
+            success:function(result){
+                console.log(result);
+                if(result=="用户名或邮箱不正确"){
+                    $("#warning").html("<h5>用户名或邮箱不正确</h5>");
+                }else if(result=="修改用户密码成功"){
+                    alert("修改用户密码成功!");
+                    location.href=("login.html");
+                }
+            }
+        })
+    }else if(name==''){
+        $("#warning").html("<h5>用戶名不能空缺！</h5>");
+      }else if(email==''){
+        $("#warning").html("<h5>邮箱不能空缺！</h5>");
+      }else{
+        $("#warning").html("<h5>新密码不能空缺！</h5>");
+      }
+})
